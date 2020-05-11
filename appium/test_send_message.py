a9a1975a817d11b8ac94949f051ef5e151b7b340
Thomas Carpentier <tcarpentier@genymobile.com>
2020-05-11 16:53:46 +0200

    4 devices for BUG

--- appium/test_send_message.py
+++ appium/test_send_message.py
@@ -4,7 +4,11 @@ from appium import webdriver
 
 
 @pytest.mark.parametrize(
-    "udid, systemPort", [("localhost:4321", "8201"), ("localhost:4322", "8202"),]
+    "udid, systemPort", [
+        ("localhost:4321", "8201"),
+        ("localhost:4322", "8202"),
+        ("localhost:4323", "8203"),
+        ("localhost:4324", "8204")]
 )
 def test_send_message(udid, systemPort, getAPKPath):
 
@@ -24,7 +28,7 @@ def test_send_message(udid, systemPort, getAPKPath):
         'new UiSelector().resourceId("com.tcarpentier.myAndroidApp:id/editText")'
     )
     txt_ipt.clear()
-    txt_ipt.send_keys("It's a trap!")
+    txt_ipt.send_keys("Hello Bitrise User Group!")
 
     send_btn = driver.find_element_by_android_uiautomator(
         'new UiSelector().resourceId("com.tcarpentier.myAndroidApp:id/button")'
@@ -34,7 +38,7 @@ def test_send_message(udid, systemPort, getAPKPath):
     current_text = driver.find_element_by_android_uiautomator(
         'new UiSelector().resourceId("com.tcarpentier.myAndroidApp:id/textView")'
     )
-    assert current_text.text == "It's a trap!"
+    assert current_text.text == "Hello Bitrise User Group!"
 
     driver.back()
 
