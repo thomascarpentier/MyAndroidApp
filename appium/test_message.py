2405a558c89516f5d2d5c44416c595cc1911da2d
Thomas Carpentier <tcarpentier@genymobile.com>
2020-12-09 14:39:48 +0100

    update test

--- appium/test_message.py
+++ appium/test_message.py
@@ -4,23 +4,6 @@ from appium import webdriver
 
 
 class TestDemoApp:
-    @pytest.fixture(scope="module")
-    def appium(self):
-        desired_caps = {
-            "platformName": "Android",
-            "deviceName": "Genymotion Cloud SaaS Devices",
-            "app": getAPKPath,
-            "automationName": "UiAutomator2",
-            "noReset": True,
-        }
-        url = "http://localhost:4723/wd/hub"
-        appium_ = webdriver.Remote(url, desired_caps)
-
-        yield appium_
-        # teardown appium
-        appium_.quit()
-
-
 
     @pytest.mark.parametrize("message", [
         "This is a demo",
@@ -28,23 +11,36 @@ class TestDemoApp:
         "Search @genymotion on twitter",
         "Text with numbers : 02015453321",
     ])
-    def test_send_message(self, appium, message):
+    def test_send_message(self, message, getAPKPath):
 
-        self.driver = appium
-        txt_ipt = self.driver.find_element_by_android_uiautomator(
+
+        capabilities = {
+            "platformName": "Android",
+            "deviceName": "Genymotion PaaS Device",
+            "app": getAPKPath,
+            "automationName": "UiAutomator2",
+            "noReset": True,
+        }
+        url = "http://localhost:4723/wd/hub"
+        driver = webdriver.Remote(url, capabilities)
+
+        txt_ipt = driver.find_element_by_android_uiautomator(
             'new UiSelector().resourceId("com.tcarpentier.myAndroidApp:id/editText")'
         )
         txt_ipt.clear()
         txt_ipt.send_keys(message)
 
-        send_btn = self.driver.find_element_by_android_uiautomator(
+        send_btn = driver.find_element_by_android_uiautomator(
             'new UiSelector().resourceId("com.tcarpentier.myAndroidApp:id/button")'
         )
         send_btn.click()
 
-        current_text = self.driver.find_element_by_android_uiautomator(
+        current_text = driver.find_element_by_android_uiautomator(
             'new UiSelector().resourceId("com.tcarpentier.myAndroidApp:id/textView")'
         )
         assert current_text.text == message
 
-        self.driver.back()
+        driver.back()
+
+        # teardown appium
+        driver.quit()
