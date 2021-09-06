66edfe0707b5593749b8035d5618dc2c77d092ce
Thomas Carpentier <tcarpentier@genymobile.com>
2021-09-06 17:39:46 +0200

    Run tests on 4 devices

--- appium/test_send_message.py
+++ appium/test_send_message.py
@@ -9,6 +9,7 @@ import time
         ("localhost:4321", "8201"),
         ("localhost:4322", "8202"),
         ("localhost:4323", "8203"),
+        ("localhost:4324", "8204"),
         ]
 )
 def test_send_message(udid, systemPort, getAPKPath):
